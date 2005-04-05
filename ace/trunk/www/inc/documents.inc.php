93eb752d28d44b54dc55ad38242fa8ef5e07d4e5
sim <sim@cc186578-7ef1-0310-b290-876586ca5d03>
2005-04-05 19:16:17 +0000

    added link to pflichtenheft

--- ace/trunk/www/inc/documents.inc.php
+++ ace/trunk/www/inc/documents.inc.php
@@ -4,7 +4,7 @@
   <li>
     <a href="http://ace.iserver.ch:81/repos/ace/ace/trunk/doc/pdf/projektantrag.pdf">Projektantrag (German)</a>
   </li>
-  <li><a href="http://ace.iserver.ch:81/repos/ace/ace/trunk/doc/pdf/pflichtenheft.pdf">Projekthandbuch (German)</a></li>
+  <li><a href="http://ace.iserver.ch:81/repos/ace/ace/trunk/doc/pdf/pflichtenheft.pdf">Pflichtenheft (German)</a></li>
+  <li>Projekhandbuch (German)</li>
   <li>Projektplan (German)</li>
-  <li>Pflichtenheft (German)</li>
 </ul>
