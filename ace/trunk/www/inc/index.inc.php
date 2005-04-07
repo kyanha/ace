f0912d7a09a994cba4c351ff984a837ee17a34dd
sim <sim@cc186578-7ef1-0310-b290-876586ca5d03>
2005-04-07 12:49:52 +0000

    added docs.php

--- ace/trunk/www/inc/index.inc.php
+++ ace/trunk/www/inc/index.inc.php
@@ -1,3 +1,3 @@
-<p>ACE is a collaborative editor.</p>
-
 <h2>What is ACE?</h2>
+
+<p>ACE is a collaborative text editor. It allows to edit text documents in collaboration with other users in real-time. The changes of other users can be observed.</p>
