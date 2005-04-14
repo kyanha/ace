0493785e813bdc2658306fac0c4675163ba6de0f
sim <sim@cc186578-7ef1-0310-b290-876586ca5d03>
2005-04-14 19:43:46 +0000

    added latest documents to website

--- ace/trunk/www/inc/documents.inc.php
+++ ace/trunk/www/inc/documents.inc.php
@@ -5,6 +5,6 @@
     <a href="http://ace.iserver.ch:81/repos/ace/ace/trunk/doc/pdf/projektantrag.pdf">Projektantrag (German)</a>
   </li>
   <li><a href="http://ace.iserver.ch:81/repos/ace/ace/trunk/doc/pdf/pflichtenheft.pdf">Pflichtenheft (German)</a></li>
-  <li>Projekhandbuch (German)</li>
-  <li>Projektplan (German)</li>
+  <li><a href="http://ace.iserver.ch:81/repos/ace/ace/trunk/doc/pdf/projekthandbuch.pdf">Projekhandbuch (German)</a></li>
+  <li><a href="http://ace.iserver.ch:81/repos/ace/ace/trunk/doc/pdf/projektplan.pdf">Projektplan (German)</a></li>
 </ul>
