56b39bac43bf02afc89d2ef513ab1a13c4b8baff
sim <sim@cc186578-7ef1-0310-b290-876586ca5d03>
2005-05-03 14:18:20 +0000

    basic design of algorithm and related objects

---
+++ ace/trunk/src/java/ch/iserver/ace/algorithm/Algorithm.java
@@ -0,0 +1,18 @@
+package ch.iserver.ace.algorithm;
+
+import ch.iserver.ace.DocumentModel;
+import ch.iserver.ace.Operation;
+
+public interface Algorithm {
+
+	public Request generateRequest(Operation op);
+	public void receiveRequest(Request req);
+	public void siteAdded(int siteId);
+	public void siteRemoved(int siteId);
+	
+	public void setDocument(DocumentModel doc);
+	public DocumentModel getDocument();
+	
+	public void setInitialTimestamp(Timestamp timestamp);
+	
+}
