1080cf036123a6f7265ffca72582af26cb2edf4d
sim <sim@cc186578-7ef1-0310-b290-876586ca5d03>
2005-05-04 08:52:17 +0000

    - added build file (using maven style dependecies) - adapted classpath to use maven repository - added undo to algorithm

--- ace/trunk/src/java/ch/iserver/ace/algorithm/Algorithm.java
+++ ace/trunk/src/java/ch/iserver/ace/algorithm/Algorithm.java
@@ -5,8 +5,10 @@ import ch.iserver.ace.Operation;
 
 public interface Algorithm {
 
+	public Request undo();
 	public Request generateRequest(Operation op);
 	public void receiveRequest(Request req);
+	
 	public void siteAdded(int siteId);
 	public void siteRemoved(int siteId);
 	
