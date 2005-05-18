fb275a52e5acc6e919cf8ef89bb5273c0a354939
zbinl <zbinl@cc186578-7ef1-0310-b290-876586ca5d03>
2005-05-18 11:37:42 +0000

    added 2 more links.

--- ace/trunk/www/inc/links.inc.php
+++ ace/trunk/www/inc/links.inc.php
@@ -5,4 +5,12 @@
   <a href="http://www.codingmonkeys.de/subethaedit/">SubEthaEdit</a>
   - Mac OS X based collaborative editor
  </li>
+   <li>
+  <a href="http://www.cit.gu.edu.au/~scz/sigce/">SIGCE</a>
+  - An International Special Interest Group on Collaborative Editing.
+ </li>
+  <li>
+  <a href="http://charlie.dgrc.crc.ca/~sylvie/collwrite.html">Collaborative writing</a>
+  - A link collection on collaborative writing.
+ </li>
 </ul>
