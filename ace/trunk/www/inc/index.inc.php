58b33d004dfc6f3e86f8bbb9d695453024838a9b
zbinl <zbinl@cc186578-7ef1-0310-b290-876586ca5d03>
2005-05-18 11:29:12 +0000

    added link for algorithm report.

--- ace/trunk/www/inc/index.inc.php
+++ ace/trunk/www/inc/index.inc.php
@@ -1,3 +1,6 @@
 <h2>What is ACE?</h2>
 
 <p>ACE is a plattform-independent, collaborative text editor. ACE represents a real-time cooperative editing system that allows multiple dispersed users to view and edit a shared text document at the same time.</p>
+<p>
+Read our evaluation report on operational transformation algorithms (<a href="http://ace.iserver.ch:81/repos/ace/ace/trunk/doc/pdf/algorithm.pdf">PDF</a>).
+</p>
