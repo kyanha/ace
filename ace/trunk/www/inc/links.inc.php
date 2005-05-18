f1964323b42831d754f9f09e296433ac310b35e3
zbinl <zbinl@cc186578-7ef1-0310-b290-876586ca5d03>
2005-05-18 11:41:31 +0000

    minor changes.

--- ace/trunk/www/inc/links.inc.php
+++ ace/trunk/www/inc/links.inc.php
@@ -7,10 +7,10 @@
  </li>
    <li>
   <a href="http://www.cit.gu.edu.au/~scz/sigce/">SIGCE</a>
-  - An International Special Interest Group on Collaborative Editing.
+  - An interest group on collaborative editing.
  </li>
   <li>
   <a href="http://charlie.dgrc.crc.ca/~sylvie/collwrite.html">Collaborative writing</a>
-  - A link collection on collaborative writing.
+  - A link collection.
  </li>
 </ul>
