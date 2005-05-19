c3829ea0eaa61446429d6a87a66fd1a929464e49
zbinl <zbinl@cc186578-7ef1-0310-b290-876586ca5d03>
2005-05-19 07:45:26 +0000

    open new window when clicking on a link.

--- ace/trunk/www/inc/links.inc.php
+++ ace/trunk/www/inc/links.inc.php
@@ -2,15 +2,15 @@
 
 <ul>
  <li>
-  <a href="http://www.codingmonkeys.de/subethaedit/">SubEthaEdit</a>
+  <a href="http://www.codingmonkeys.de/subethaedit/" target="_blank">SubEthaEdit</a>
   - Mac OS X based collaborative editor
  </li>
    <li>
-  <a href="http://www.cit.gu.edu.au/~scz/sigce/">SIGCE</a>
+  <a href="http://www.cit.gu.edu.au/~scz/sigce/" target="_blank">SIGCE</a>
   - An interest group on collaborative editing.
  </li>
   <li>
-  <a href="http://charlie.dgrc.crc.ca/~sylvie/collwrite.html">Collaborative writing</a>
+  <a href="http://charlie.dgrc.crc.ca/~sylvie/collwrite.html" target="_blank">Collaborative writing</a>
   - A link collection.
  </li>
 </ul>
