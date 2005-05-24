ccaffca6f6108cfae7cfde09acef13d4e32a8123
sim <sim@cc186578-7ef1-0310-b290-876586ca5d03>
2005-05-24 15:16:03 +0000

    initial version of jgroups and bonjour prototypes

---
+++ prototypes/trunk/network/jgroups/src/java/ch/iserver/ace/net/jgroups/JGroupsTest.java
@@ -0,0 +1,75 @@
+/*
+ * $Id$
+ *
+ * ace - a collaborative editor
+ * Copyright (C) 2005 Mark Bigler, Simon Raess, Lukas Zbinden
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package ch.iserver.ace.net.jgroups;
+
+import org.jgroups.ChannelException;
+import org.jgroups.JChannel;
+import org.jgroups.Message;
+import org.jgroups.TimeoutException;
+import org.jgroups.View;
+
+/**
+ * 
+ */
+public class JGroupsTest {
+
+	public JGroupsTest(JChannel channel, String group) throws ChannelException {
+		channel.connect(group);
+		System.out.println("... connected to " + group);
+		System.out.println("... local address is " + channel.getLocalAddress());
+		System.out.println("... waiting for messages");
+		
+		try {
+			Object obj = channel.receive(0);
+			if (obj instanceof Message) {
+				Message msg = (Message) obj;
+				System.out.println("... got message: " + msg);
+			} else if (obj instanceof View) {
+				View view = (View) obj;
+				System.out.println("... got view: " + view);
+			}
+		} catch (TimeoutException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		String props = "UDP(mcast_addr=228.1.2.3;mcast_port=45566;ip_ttl=32):"
+				+ "PING(timeout=3000;num_initial_members=6):"
+				+ "FD(timeout=5000):" + "VERIFY_SUSPECT(timeout=1500):"
+				+ "pbcast.STABLE(desired_avg_gossip=10000):"
+				+ "pbcast.NAKACK(gc_lag=10;retransmit_timeout=3000):"
+				+ "UNICAST:" 
+				+ "FRAG:"
+				+ "pbcast.GMS(join_timeout=5000;"
+				+ "join_retry_timeout=2000;shun=false;print_local_addr=false)";
+		JChannel channel;
+		try {
+			//channel = new JChannel(props);
+			channel = new JChannel();
+			System.out.println("... created channel");
+			new JGroupsTest(channel, args[0]);
+		} catch (ChannelException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ prototypes/trunk/network/jgroups/src/java/ch/iserver/ace/net/jgroups/SimpleChat.java
@@ -0,0 +1,228 @@
+/*
+ * $Id$
+ *
+ * ace - a collaborative editor
+ * Copyright (C) 2005 Mark Bigler, Simon Raess, Lukas Zbinden
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package ch.iserver.ace.net.jgroups;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import org.jgroups.Address;
+import org.jgroups.Channel;
+import org.jgroups.ChannelException;
+import org.jgroups.JChannel;
+import org.jgroups.MembershipListener;
+import org.jgroups.Message;
+import org.jgroups.MessageListener;
+import org.jgroups.View;
+import org.jgroups.blocks.PullPushAdapter;
+
+/**
+ * 
+ */
+public class SimpleChat extends JFrame {
+	private JButton joinButton;
+	private JButton leaveButton;
+	
+	private JTextArea messageArea;
+	
+	private JTextField messageField;
+	private JButton sendButton;
+	
+	private Channel channel;
+	private PullPushAdapter adapter;
+	
+	private String group;
+	private ChatConfig config;
+	
+	public SimpleChat(ChatConfig config) {
+		super();		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		this.config = config;
+		setTitle(createTitle(config));
+
+		// content pane
+		JPanel pane = new JPanel(new BorderLayout());
+
+		JPanel buttonPane = new JPanel(new FlowLayout());
+		joinButton = new JButton("join");
+		joinButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					String name = JOptionPane.showInputDialog("Specify group name");
+					if (name != null) {
+						connect(name);
+					}
+				} catch (ChannelException ex) {
+					handleException(ex);
+				}
+			}
+		});
+		leaveButton = new JButton("leave");
+		leaveButton.setEnabled(false);
+		leaveButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				disconnect();
+			}
+		});
+		buttonPane.add(joinButton);
+		buttonPane.add(leaveButton);
+		
+		JPanel messagePane = new JPanel(new BorderLayout());
+		messageArea = new JTextArea();
+		messageArea.setEditable(false);
+		messagePane.add(messageArea, BorderLayout.CENTER);
+		
+		JPanel inputPane = new JPanel(new BorderLayout());
+		ActionListener sendListener = new ActionListener() {		
+			public void actionPerformed(ActionEvent e) {
+				try {
+					send(messageField.getText());
+				} catch (ChannelException ex) {
+					handleException(ex);
+				} finally {
+					messageField.setText("");
+				}
+			}
+		};
+		messageField = new JTextField();
+		messageField.setEnabled(false);
+		messageField.addActionListener(sendListener);
+		sendButton = new JButton("send");
+		sendButton.setEnabled(false);
+		sendButton.addActionListener(sendListener);
+		inputPane.add(messageField, BorderLayout.CENTER);
+		inputPane.add(sendButton, BorderLayout.EAST);
+		
+		pane.add(buttonPane, BorderLayout.NORTH);
+		pane.add(messagePane, BorderLayout.CENTER);
+		pane.add(inputPane, BorderLayout.SOUTH);
+		setContentPane(pane);
+		
+		setSize(500, 400);
+		setVisible(true);
+	}
+	
+	private String createTitle(ChatConfig config) {
+		StringBuffer buf = new StringBuffer("SimpleChat - JGroups (");
+		buf.append(config.getUserName());
+		if (group != null) {
+			buf.append(" @ ");
+			buf.append(group);
+		}
+		buf.append(")");
+		return buf.toString();
+	}
+	
+	private void handleException(Exception e) {
+		e.printStackTrace();
+	}
+	
+	public void doEnable(boolean enable) {
+		joinButton.setEnabled(!enable);
+		leaveButton.setEnabled(enable);
+		sendButton.setEnabled(enable);
+		messageField.setEnabled(enable);
+	}
+	
+	public void connect(String name) throws ChannelException {
+		try {
+			this.group = name;
+			channel = new JChannel();
+			channel.setOpt(Channel.GET_STATE_EVENTS, new Boolean(true));
+			channel.connect(name);
+			System.out.println("... trying to get state");
+			if (channel.getState(null, 5000)) {
+				System.out.println("... got state");
+			}
+			adapter = new PullPushAdapter(channel);
+			adapter.setListener(new MyMessageListener());
+			adapter.addMembershipListener(new MyMembershipListener());
+			setTitle(createTitle(config));
+		} finally {
+			doEnable(true);
+		}
+	}
+	
+	public void send(String message) throws ChannelException {
+		if (channel != null) {
+			String username = config.getUserName();
+			Message msg = new Message(null, null, username + "> " + message);
+			channel.send(msg);
+		}
+	}
+	
+	public void disconnect() {
+		try {
+			adapter.stop();
+			channel.disconnect();
+			channel.close();
+		} finally {
+			group = null;
+			adapter = null;
+			channel = null;
+			doEnable(false);
+		}
+	}
+		
+	private class MyMessageListener implements MessageListener {
+		public byte[] getState() {
+			System.out.println("*** get state");
+			return messageArea.getText().getBytes();
+		}
+	
+		public void receive(Message message) {
+			Object obj = message.getObject();
+			messageArea.append(obj.toString() + "\n");
+		}
+	
+		public void setState(byte[] state) {
+			System.out.println("*** set state");
+			messageArea.setText(new String(state));
+		}
+	}
+	
+	private class MyMembershipListener implements MembershipListener {
+		public void block() {
+			
+		}
+		public void suspect(Address addr) {
+			System.out.println("... suspect " + addr);
+		}
+		public void viewAccepted(View view) {
+			System.out.println("... view accepted " + view);
+		}
+	}
+	
+	public static void main(String[] args) {
+		SimpleChat chat = new SimpleChat(new SimpleChatConfig());
+	}
+	
+}
