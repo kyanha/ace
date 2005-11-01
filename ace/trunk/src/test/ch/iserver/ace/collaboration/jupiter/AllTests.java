ceb5bd88578867f6ff6dba3c1410cd5c42d4445b
sim <sim@cc186578-7ef1-0310-b290-876586ca5d03>
2005-11-01 13:33:36 +0000

    - added tests for RequestCommand and CaretUpdateCommand

--- ace/trunk/src/test/ch/iserver/ace/collaboration/jupiter/AllTests.java
+++ ace/trunk/src/test/ch/iserver/ace/collaboration/jupiter/AllTests.java
@@ -1,3 +1,24 @@
+/*
+ * $Id$
+ *
+ * ace - a collaborative editor
+ * Copyright (C) 2005 Mark Bigler, Simon Raess, Lukas Zbinden
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
 package ch.iserver.ace.collaboration.jupiter;
 
 import junit.framework.Test;
@@ -9,6 +30,8 @@ public class AllTests {
 		TestSuite suite = new TestSuite(
 						"Test for ch.iserver.ace.collaboration.jupiter");
 		//$JUnit-BEGIN$
+		suite.addTestSuite(CaretUpdateCommandTest.class);
+		suite.addTestSuite(RequestCommandTest.class);
 		suite.addTestSuite(AlgorithmWrapperImplTest.class);
 		//$JUnit-END$
 		return suite;
