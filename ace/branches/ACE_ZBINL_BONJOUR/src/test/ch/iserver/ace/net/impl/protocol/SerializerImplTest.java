f5669f2630b43ec4c14bdfd11840500818b4eb0b
zbinl <zbinl@cc186578-7ef1-0310-b290-876586ca5d03>
2005-11-09 16:21:22 +0000

    unit test for SerializerImpl

---
+++ ace/branches/ACE_ZBINL_BONJOUR/src/test/ch/iserver/ace/net/impl/protocol/SerializerImplTest.java
@@ -0,0 +1,19 @@
+package ch.iserver.ace.net.impl.protocol;
+
+import junit.framework.TestCase;
+import ch.iserver.ace.net.impl.NetworkConstants;
+
+public class SerializerImplTest extends TestCase {
+
+	private static final String EXPECTED_PUBLISHED_DOCUMENTS = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<ace><request><query type=\"doc\"/></request></ace>";
+	
+	public void testCreateQueryForPublishedDocuments() throws Exception {
+		Serializer serializer = new SerializerImpl();
+		
+		byte[] data = serializer.createQuery(Serializer.PUBLISHED_DOCUMENTS);
+		String actual = new String(data, NetworkConstants.DEFAULT_ENCODING);
+		
+		assertEquals(actual, EXPECTED_PUBLISHED_DOCUMENTS);
+	}
+	
+}
