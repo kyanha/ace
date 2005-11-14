a7ebc4b0c93cd31c7ae53cc2cf26bf5a42dbabc7
sim <sim@cc186578-7ef1-0310-b290-876586ca5d03>
2005-11-14 11:46:04 +0000

    added EnhancerTask (new approach to enhancing Info.plist)

---
+++ dependency-task/trunk/src/ch/iserver/ace/ant/dependency/EnhancerTask.java
@@ -0,0 +1,118 @@
+package ch.iserver.ace.ant.dependency;
+
+import java.io.File;
+import java.util.Set;
+import java.util.TreeSet;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.sax.SAXTransformerFactory;
+import javax.xml.transform.sax.TransformerHandler;
+import javax.xml.transform.stream.StreamResult;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.DirectoryScanner;
+import org.apache.tools.ant.Task;
+import org.apache.tools.ant.types.FileSet;
+import org.apache.tools.ant.types.PatternSet;
+import org.apache.tools.ant.types.selectors.FilenameSelector;
+
+public class EnhancerTask extends Task {
+	
+	private PatternSet patterns = new PatternSet();
+	
+	private FileSet fileset;
+	
+	private File source;
+	
+	private File target;
+	
+	public EnhancerTask() {
+		
+	}
+	
+	public FileSet getFileset() {
+		return fileset;
+	}
+	
+	public void addFileset(FileSet fileset) {
+		this.fileset = fileset;
+	}
+	
+	public File getSource() {
+		return source;
+	}
+	
+	public void setSource(File source) {
+		this.source = source;
+	}
+	
+	public File getTarget() {
+		return target;
+	}
+	
+	public void setTarget(File target) {
+		this.target = target;
+	}
+	
+	public PatternSet.NameEntry createExclude() {
+		return patterns.createExclude();
+	}
+	
+	protected String[] getExcludes() {
+		return patterns.getExcludePatterns(getProject());
+	}
+	
+	public void execute() throws BuildException {
+		Set entries = new TreeSet();
+		DirectoryScanner scanner = getFileset().getDirectoryScanner(getProject());
+		scanner.scan();
+
+		String[] files = scanner.getIncludedFiles();
+		for (int i = 0; i < files.length; i++) {
+			File source = new File(files[i]);
+			if (isIncluded(source)) {
+				String name = source.getName();
+				entries.add(name);
+			}
+		}
+		
+		processDependencies(entries);
+	}
+	
+	private boolean isIncluded(File source) {
+		FilenameSelector selector = new FilenameSelector();
+		String[] excludes = getExcludes();
+		for (int i = 0; i < excludes.length; i++) {
+			String exclude = excludes[i];
+			selector.setName(exclude);
+			if (selector.isSelected(source.getParentFile(), source.getName(), source)) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	protected void processDependencies(Set dependencies) {
+		try {
+			SAXTransformerFactory sax = (SAXTransformerFactory) SAXTransformerFactory.newInstance();
+			TransformerHandler handler = sax.newTransformerHandler();
+			Transformer serializer = handler.getTransformer();
+			serializer.setOutputProperty(OutputKeys.INDENT,"yes");
+			handler.setResult(new StreamResult(getTarget()));
+			
+			PListEnhancer enhancer = new PListEnhancer(handler, dependencies);
+			
+			SAXParserFactory factory = SAXParserFactory.newInstance();
+			factory.setNamespaceAware(true);
+			SAXParser parser = factory.newSAXParser();
+			parser.setProperty("http://xml.org/sax/properties/lexical-handler", enhancer);
+			parser.parse(getSource(), enhancer);
+		} catch (Exception e) {
+			throw new BuildException("enhancing source failed", e);
+		}
+	}
+		
+}
