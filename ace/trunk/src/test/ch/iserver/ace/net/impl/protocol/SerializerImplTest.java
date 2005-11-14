bf622101813e7d29b81e556df34f7a1417d318f2
zbinl <zbinl@cc186578-7ef1-0310-b290-876586ca5d03>
2005-11-14 14:30:30 +0000

    added two more tests for publish and conceal messages

--- ace/trunk/src/test/ch/iserver/ace/net/impl/protocol/SerializerImplTest.java
+++ ace/trunk/src/test/ch/iserver/ace/net/impl/protocol/SerializerImplTest.java
@@ -6,20 +6,10 @@ import java.util.List;
 import junit.framework.TestCase;
 import ch.iserver.ace.DocumentDetails;
 import ch.iserver.ace.net.impl.NetworkConstants;
+import ch.iserver.ace.net.impl.NetworkServiceImpl;
 import ch.iserver.ace.net.impl.PublishedDocument;
 
 public class SerializerImplTest extends TestCase {
-
-	private static final String EXPECTED_QUERY = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<ace><request><query type=\"docs\"/></request></ace>";
-	
-	private static final String EXPECTED_RESPONSE = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-	"<ace><response>" +
-	"<publishedDocs>" +
-	"<doc id=\"WERS24-RE2\" name=\"testfile.txt\"/>" +
-	"<doc id=\"ADSFBW-45S\" name=\"meeting2.txt\"/>" +
-	"<doc id=\"23SSWD-3ED\" name=\"notes232.txt\"/>" +
-	"</publishedDocs>" +
-	"</response></ace>";
 	
 	public void testCreateQueryForPublishedDocuments() throws Exception {
 		Serializer serializer = SerializerImpl.getInstance();
@@ -47,8 +37,62 @@ public class SerializerImplTest extends TestCase {
 		assertEquals(EXPECTED_RESPONSE, actual);
 	}
 	
-	public void testCreateNotification() throws Exception {
-		//fail("to be implemented");
+	public void testCreateNotificationPublish() throws Exception {
+		Serializer serializer = SerializerImpl.getInstance();
+		
+		PublishedDocument doc = new PublishedDocument("WERS24-RE2", null, new DocumentDetails("testfile.txt"), null);
+		
+		byte[] data = serializer.createNotification(ProtocolConstants.PUBLISH, doc);
+		String actual = new String(data, NetworkConstants.DEFAULT_ENCODING);
+
+		String userId = NetworkServiceImpl.getInstance().getUserId();
+		String xmlPublish = XML_PUBLISH_1+userId+XML_PUBLISH_2;
+		
+		assertEquals(xmlPublish, actual);
+		
 	}
 	
+	public void testCreateNotificationConceal() throws Exception {
+		Serializer serializer = SerializerImpl.getInstance();
+		
+		PublishedDocument doc = new PublishedDocument("WERS24-RE2", null, new DocumentDetails("testfile.txt"), null);
+		
+		byte[] data = serializer.createNotification(ProtocolConstants.CONCEAL, doc);
+		String actual = new String(data, NetworkConstants.DEFAULT_ENCODING);
+
+		String userId = NetworkServiceImpl.getInstance().getUserId();
+		String xmlPublish = XML_CONCEAL_1+userId+XML_CONCEAL_2;
+		
+		assertEquals(xmlPublish, actual);
+	}
+	
+	private static final String EXPECTED_QUERY = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+			"<ace><request>" +
+			"<query type=\"docs\"/>" +
+			"</request></ace>";
+	
+	private static final String EXPECTED_RESPONSE = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+			"<ace><response>" +
+			"<publishedDocs>" +
+			"<doc id=\"WERS24-RE2\" name=\"testfile.txt\"/>" +
+			"<doc id=\"ADSFBW-45S\" name=\"meeting2.txt\"/>" +
+			"<doc id=\"23SSWD-3ED\" name=\"notes232.txt\"/>" +
+			"</publishedDocs>" +
+			"</response></ace>";
+	
+	private static final String XML_PUBLISH_1 = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+			"<ace><notification>" +
+			"<publishDocs userid=\"";
+	
+	private static final String XML_PUBLISH_2 = "\"><doc id=\"WERS24-RE2\" name=\"testfile.txt\"/>" +
+			"</publishDocs>" +
+			"</notification></ace>";
+	
+	private static final String XML_CONCEAL_1 = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+			"<ace><notification>" +
+			"<concealDocs userid=\"";
+	
+	private static final String XML_CONCEAL_2 = "\"><doc id=\"WERS24-RE2\"/>" +
+			"</concealDocs>" +
+			"</notification></ace>";
 }
