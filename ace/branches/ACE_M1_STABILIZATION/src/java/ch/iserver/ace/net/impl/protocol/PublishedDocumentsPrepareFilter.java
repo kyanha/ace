7feb73ed77957741285a463b7d2e18c941b0d83f
zbinl <zbinl@cc186578-7ef1-0310-b290-876586ca5d03>
2005-11-17 12:32:12 +0000

    initial checkin

---
+++ ace/branches/ACE_M1_STABILIZATION/src/java/ch/iserver/ace/net/impl/protocol/PublishedDocumentsPrepareFilter.java
@@ -0,0 +1,74 @@
+/*
+ * $Id$
+ *
+ * ace - a collaborative editor
+ * Copyright (C) 2005 Mark Bigler, Simon Raess, Lukas Zbinden
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package ch.iserver.ace.net.impl.protocol;
+
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.beepcore.beep.core.ReplyListener;
+
+import ch.iserver.ace.net.impl.NetworkServiceImpl;
+import ch.iserver.ace.net.impl.RemoteUserProxyExt;
+import ch.iserver.ace.net.impl.protocol.DocumentDiscoveryPrepareFilter.QueryInfo;
+
+/**
+ *
+ */
+public class PublishedDocumentsPrepareFilter extends AbstractRequestFilter {
+
+private static Logger LOG = Logger.getLogger(PublishedDocumentsPrepareFilter.class);
+	
+	private Serializer serializer;
+	private SessionManager manager;
+	private ReplyListener listener;
+	
+	public PublishedDocumentsPrepareFilter(RequestFilter successor, Serializer serializer, ReplyListener listener) {
+		super(successor);
+		this.serializer = serializer;
+		//TODO: could use NullReplyListener here
+		this.listener = listener;
+		manager = SessionManager.getInstance();
+	}
+	
+	public void process(Request request) {
+		LOG.info("--> process("+request+")");
+		if (request.getType() == ProtocolConstants.SEND_DOCUMENTS) {
+			processImpl(request);
+		} else { //forward
+			super.process(request);
+		}
+		LOG.info("<-- process()");
+	}
+
+	private void processImpl(Request request) {
+		try {			
+			RemoteUserProxyExt user = (RemoteUserProxyExt)request.getPayload(); 
+			RemoteUserSession session = manager.createSession(user); 
+			ParticipantConnectionExt connection = session.getConnection();
+			Map publishedDocs = NetworkServiceImpl.getInstance().getPublishedDocuments();
+			byte[] message = serializer.createNotification(ProtocolConstants.SEND_DOCUMENTS, publishedDocs);	
+			connection.send(message, null, listener);
+		} catch (Exception e) {
+			LOG.error("process problem ["+e.getMessage()+"]");
+		}
+	}
+}
