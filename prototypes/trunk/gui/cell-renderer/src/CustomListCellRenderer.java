b501718b64e7842d23dd67c3f16a94c434928806
sim <sim@cc186578-7ef1-0310-b290-876586ca5d03>
2005-11-18 07:28:24 +0000

    added icon

--- prototypes/trunk/gui/cell-renderer/src/CustomListCellRenderer.java
+++ prototypes/trunk/gui/cell-renderer/src/CustomListCellRenderer.java
@@ -3,6 +3,9 @@ import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Graphics;
 
+import javax.swing.BorderFactory;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.ListCellRenderer;
@@ -10,14 +13,22 @@ import javax.swing.ListCellRenderer;
 public class CustomListCellRenderer extends JPanel implements ListCellRenderer {
 
 	private Object value;
+
+	private Icon icon;
 	
+	public CustomListCellRenderer() {
+		this.icon = new ImageIcon(getClass().getResource("view_file_local.gif"));
+	}
+
 	public Component getListCellRendererComponent(JList list, Object value,
 					int index, boolean isSelected, boolean cellHasFocus) {
 		this.value = value;
 		if (isSelected) {
 			this.setBackground(Color.LIGHT_GRAY);
+			this.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY));
 		} else {
 			this.setBackground(Color.WHITE);
+			this.setBorder(BorderFactory.createEmptyBorder(1,1,1,1));
 		}
 		return this;
 	}
@@ -29,8 +40,9 @@ public class CustomListCellRenderer extends JPanel implements ListCellRenderer {
 	protected void paintComponent(Graphics g) {
 		g.setColor(getBackground());
 		g.fillRect(0, 0, getWidth(), getHeight());
+		icon.paintIcon(this, g, 0, 2);
 		g.setColor(Color.BLACK);
-		g.drawString(value.toString(), 5, 15);
+		g.drawString(value.toString(), 25, 15);
 	}
 
 }
