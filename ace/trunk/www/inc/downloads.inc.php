979f2faf993d6fc40611182d41bade48ec21e35c
sim <sim@cc186578-7ef1-0310-b290-876586ca5d03>
2005-11-20 12:06:11 +0000

    redirect downloads to downloads.php

--- ace/trunk/www/inc/downloads.inc.php
+++ ace/trunk/www/inc/downloads.inc.php
@@ -5,7 +5,7 @@
 
 <h3>Milestone 1</h3>
 <ul>
- <li><a href="downloads/ace-win-m1.exe">Windows Installer</a></li>
- <li><a href="downloads/ace-osx-m1.dmg">OS X Disk Image</a></li>
- <li><a href="downloads/ace-m1.tar.gz">Other Platforms</a></li>
+ <li><a href="downloads.php?id=0">Windows Installer</a></li>
+ <li><a href="downloads.php?id=1">OS X Disk Image</a></li>
+ <li><a href="downloads.php?id=2">Other Platforms</a></li>
 </ul>
