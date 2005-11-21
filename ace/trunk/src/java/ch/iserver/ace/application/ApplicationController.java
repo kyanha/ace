2d4b550d69331d5f5cf6ed1775c33fc7c6de278b
sim <sim@cc186578-7ef1-0310-b290-876586ca5d03>
2005-11-21 16:04:39 +0000

    added javadoc comments

--- ace/trunk/src/java/ch/iserver/ace/application/ApplicationController.java
+++ ace/trunk/src/java/ch/iserver/ace/application/ApplicationController.java
@@ -22,32 +22,86 @@
 package ch.iserver.ace.application;
 
 /**
- *
+ * The Application Controller is the central class handling application level
+ * events like for instance quit, showing different dialogs, saving, opening, 
+ * and closing documents.
+ * 
+ * <p>The ApplicationController is further used by Customizer classes. For
+ * instance on Mac OS X some methods in this interface are used to improve
+ * the user experience on OS X.</p>
  */
 public interface ApplicationController {
 	
+	/**
+	 * Shows the applications about dialog.
+	 */
 	void showAbout();
 	
+	/**
+	 * Shows the preferences dialog.
+	 */
 	void showPreferences();
 
+	/**
+	 * Opens the passed in file in a new editor document.
+	 * 
+	 * @param filename the filename of the file to open
+	 */
 	void openFile(String filename);
 	
+	/**
+	 * Terminates the application. An implementation will typcially ask
+	 * the user about unsaved changes, thus this method does not have to
+	 * result in a shutdown because the user might cancel the shutdown
+	 * process.
+	 */
 	void quit();
 	
+	/**
+	 * Closes the currently selected document, if any. The user should
+	 * be asked to save unsaved changes.
+	 */
 	void closeDocument();
 	
+	/**
+	 * Closes all the documents. The user should be asked to save unsaved
+	 * changes.
+	 */
 	void closeAllDocuments();
 
+	/**
+	 * Shows a file open dialog which lets the user choose one or more files
+	 * to open in new editor documents.
+	 */
 	void openDocument();
 	
+	/**
+	 * Saves the currently selected document. If the document has never been
+	 * saved before, this method typically asks the user to set the filename
+	 * in a file save dialog.
+	 */
 	void saveDocument();
 	
+	/**
+	 * Saves the document under another name.
+	 */
 	void saveDocumentAs();
 
+	/**
+	 * Saves a copy of the currently selected document to a file.
+	 */
 	void saveDocumentCopy();
 	
+	/**
+	 * Saves all the open and changed files.
+	 */
 	void saveAllDocuments();
 	
+	/**
+	 * Shows a dialog which lets the user enter an IP address of another
+	 * user. After this dialog closes, an explicit discovery request
+	 * is started.
+	 */
 	void discoverUser();
 	
 }
