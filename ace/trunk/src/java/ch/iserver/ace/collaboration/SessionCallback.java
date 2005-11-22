038d7d400cd9abe812a68875aed88112318841c3
sim <sim@cc186578-7ef1-0310-b290-876586ca5d03>
2005-11-22 07:40:49 +0000

    added missing interface

---
+++ ace/trunk/src/java/ch/iserver/ace/collaboration/SessionCallback.java
@@ -0,0 +1,45 @@
+package ch.iserver.ace.collaboration;
+
+import ch.iserver.ace.CaretUpdate;
+import ch.iserver.ace.Operation;
+
+public interface SessionCallback {
+
+	/**
+	 * Notifies the session callback that a new user joined the Session.
+	 * 
+	 * @param participant the participant that joined
+	 */
+	void participantJoined(Participant participant);
+
+	/**
+	 * Notifies the session callback that a user left the Session.
+	 * 
+	 * @param participant the participant that left the Session
+	 * @param code the reason why the participant left
+	 */
+	void participantLeft(Participant participant, int code);
+
+	/**
+	 * Receives an operation from the given participant.
+	 * 
+	 * @param participant the participant that sent the operation
+	 * @param operation the operation to be applied to the document
+	 */
+	void receiveOperation(Participant participant, Operation operation);
+
+	/**
+	 * Receives a caret update from the given participant.
+	 * 
+	 * @param participant the participant that sent the CaretUpdate
+	 * @param update the caret update specification
+	 */
+	void receiveCaretUpdate(Participant participant, CaretUpdate update);
+
+	/**
+	 * @param reason
+	 * @param e
+	 */
+	void sessionFailed(int reason, Exception e);
+
+}
