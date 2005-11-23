889f2e4a027574618c64dec98698ce2f6c51dda0
sim <sim@cc186578-7ef1-0310-b290-876586ca5d03>
2005-11-23 12:53:21 +0000

    added javadoc

--- ace/trunk/src/java/ch/iserver/ace/collaboration/jupiter/server/AccessControlStrategy.java
+++ ace/trunk/src/java/ch/iserver/ace/collaboration/jupiter/server/AccessControlStrategy.java
@@ -25,10 +25,19 @@ import ch.iserver.ace.collaboration.JoinRequest;
 import ch.iserver.ace.collaboration.jupiter.PublisherConnection;
 
 /**
- *
+ * An AccessControlStrategy determines how to handle join requests. The
+ * normal implementation is to ask the publisher connection for join
+ * permission, however other schemes can be implemented.
  */
 public interface AccessControlStrategy {
 	
+	/**
+	 * Requests the strategy for permission to join the session. The request
+	 * can either be accepted or rejected.
+	 * 
+	 * @param connection the connection to the publisher of the session
+	 * @param request the join request, containing the user that wants to join
+	 */
 	void joinRequest(PublisherConnection connection, JoinRequest request);
 	
 }
