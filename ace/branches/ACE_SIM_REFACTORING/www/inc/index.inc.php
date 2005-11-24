90b3b480171325cab846e49307307a80965c6a23
sim <sim@cc186578-7ef1-0310-b290-876586ca5d03>
2005-11-24 07:21:35 +0000

    removing unused branch

--- ace/branches/ACE_SIM_REFACTORING/www/inc/index.inc.php
+++
@@ -1,7 +0,0 @@
-<h2>What is ACE?</h2>
-
-<p>ACE is a plattform-independent, collaborative text editor. ACE represents a real-time cooperative editing system that allows multiple dispersed users to view and edit a shared text document at the same time.</p>
-
-<h3>News</h3>
-<p><b>Start Diploma Project</b> <span class="date">24.10.2005</span></p>
-<p>Today, we started working on our diploma project. The deadline for this project is December 16th. Keep watching for news. The first milestone release will be available soon.</p>
--- ace/branches/ACE_SIM_REFACTORING/www/inc/topbar.inc.php
+++
@@ -1,7 +0,0 @@
-  <div id="topbar">
-   <ul>
-    <li><a href="http://ace.iserver.ch/phpicalendar/">calendar</a></li>
-    <li><a href="http://ace.iserver.ch:81/cgi-bin/trac.cgi">trac</a></li>
-    <li><a href="http://ptime.insode.ch/">pTime</a></li>
-   </ul>
-  </div>
