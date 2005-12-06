430425434a133f70caebde0642572bd2693149fd
zbinl <zbinl@cc186578-7ef1-0310-b290-876586ca5d03>
2005-12-06 12:01:17 +0000

    initial checkin (ExplicitUserDiscovery and DataStreamHelper)

---
+++ ace/trunk/src/java/ch/iserver/ace/net/impl/discovery/ExplicitUserDiscovery.java
@@ -0,0 +1,77 @@
+/*
+ * $Id$
+ *
+ * ace - a collaborative editor
+ * Copyright (C) 2005 Mark Bigler, Simon Raess, Lukas Zbinden
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package ch.iserver.ace.net.impl.discovery;
+
+import java.net.InetAddress;
+
+import org.apache.log4j.Logger;
+
+import ch.iserver.ace.FailureCodes;
+import ch.iserver.ace.net.DiscoveryNetworkCallback;
+import ch.iserver.ace.net.impl.MutableUserDetails;
+import ch.iserver.ace.net.impl.RemoteUserProxyExt;
+import ch.iserver.ace.net.impl.RemoteUserProxyFactory;
+import ch.iserver.ace.net.impl.protocol.DiscoveryException;
+import ch.iserver.ace.util.ParameterValidator;
+import ch.iserver.ace.util.UUID;
+
+/**
+ *
+ */
+public class ExplicitUserDiscovery extends Thread {
+
+	private Logger LOG = Logger.getLogger(ExplicitUserDiscovery.class);
+	
+	private InetAddress address;
+	private int port;
+	private DiscoveryNetworkCallback callback;
+	
+	public ExplicitUserDiscovery(DiscoveryNetworkCallback callback, InetAddress address, int port) {
+		ParameterValidator.notNull("callback", callback);
+		ParameterValidator.notNull("address", address);
+		this.callback = callback;
+		this.address = address;
+		this.port = port;
+	}
+	
+	public void run() {
+		LOG.debug("--> run()");
+		MutableUserDetails details = new MutableUserDetails("discovering...", address, port);
+		String temporaryID = UUID.nextUUID();
+		RemoteUserProxyExt proxy = RemoteUserProxyFactory.getInstance().createProxy(temporaryID, details);
+		proxy.setExplicityDiscovered(true);
+		
+		try {
+			proxy.discover();
+		} catch (DiscoveryException ce) {
+			LOG.debug("could not connect to [" + address + ":" + port + "], explicit user discovery failed");
+			callback.userDiscoveryFailed(FailureCodes.DISCOVERY_FAILED,  address + ":" + port);
+			return;
+		}
+		DiscoveryManagerFactory.getDiscoveryManager(null).addUser(proxy);
+		callback.userDiscoverySucceeded();
+			
+		LOG.debug("<-- run()");
+	}
+	
+	
+}
