79c3bea2ebb8c8da720414aaba7ab61ab18191da
zbinl <zbinl@cc186578-7ef1-0310-b290-876586ca5d03>
2006-01-09 12:12:09 +0000

    fix errors

--- ace/trunk/www/inc/screenshots.inc.php
+++ ace/trunk/www/inc/screenshots.inc.php
@@ -3,14 +3,14 @@
 <p><b>Consider a collaborative editing session with three participants named Bill, Steve and Scott as depicted by the figures below. There is also a fourth user named Sam but does not particpate in the session. The document which the users collaboratively edit is <i>Collaboration.txt</i>. User Scott is the owner and publisher of it. Note that users Steve and Bill work on Windows OS, whereas Scott uses Mac OS X and Sam Linux OS.</b>
 </p>
 
-<img alt="ACE as seen by user Bill" src="images/windows_bill" width="558" height="340" align="center"/>
+<img alt="ACE as seen by user Bill" src="images/windows_bill.png" width="558" height="340" align="center"/>
 <p>ACE as seen by user Bill.</p>
 
-<img alt="ACE as seen by user Steve" src="images/windows_steve" width="558" height="340" align="center"/>
+<img alt="ACE as seen by user Steve" src="images/windows_steve.png" width="558" height="340" align="center"/>
 <p>ACE as seen by user Steve.</p>
 
-<img alt="ACE as seen by user Scott" src="images/mac_scott" width="558" height="340" align="center"/>
+<img alt="ACE as seen by user Scott" src="images/mac_scott.png" width="558" height="340" align="center"/>
 <p>ACE as seen by user Scott.</p>
 
-<img alt="ACE as seen by user Sam" src="images/linux_sam" width="558" height="340" align="center"/>
+<img alt="ACE as seen by user Sam" src="images/linux_sam.png" width="558" height="340" align="center"/>
 <p>ACE as seen by user Sam.</p>
