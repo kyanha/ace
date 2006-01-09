0bbc01e3f8b823d4b14137077d0da3696480de98
zbinl <zbinl@cc186578-7ef1-0310-b290-876586ca5d03>
2006-01-09 12:23:32 +0000

    n/a

--- ace/trunk/www/inc/screenshots.inc.php
+++ ace/trunk/www/inc/screenshots.inc.php
@@ -2,11 +2,11 @@
 
 <p><b>Consider a collaborative editing session with three participants named Bill, Steve and Scott as depicted by the figures below. There is also a fourth user named Sam but does not particpate in the session. The document which the users collaboratively edit is <i>Collaboration.txt</i>. User Scott is the owner and publisher of it. Note that users Steve and Bill work on Windows OS, whereas Scott uses Mac OS X and Sam Linux OS.</b>
 </p>
-
+<br>
 <img alt="ACE as seen by user Bill" src="images/windows_bill.png" width="480" height="293" align="center"/>
 <p>ACE as seen by user Bill.</p>
 <br>
-<img alt="ACE as seen by user Steve" src="images/windows_steve.png" width="480" height="293" align="center"/>
+<img alt="ACE as seen by user Steve" src="images/windows_steve.png" width="500" height="305" align="center"/>
 <p>ACE as seen by user Steve.</p>
 <br>
 <img alt="ACE as seen by user Scott" src="images/mac_scott.png" width="480" height="293" align="center"/>
