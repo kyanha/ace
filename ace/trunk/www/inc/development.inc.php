0a057716de87df3d2953659bc944721a8738503b
sim <sim@cc186578-7ef1-0310-b290-876586ca5d03>
2006-01-16 21:39:07 +0000

    added mailing lists to developer page

--- ace/trunk/www/inc/development.inc.php
+++ ace/trunk/www/inc/development.inc.php
@@ -17,3 +17,16 @@ issue tracker. At the moment, this issue tracker is only used internally to keep
 
 <h3>Confluence</h3>
 <p>We also use <a href="http://ace.iserver.ch:8080/confluence">Confluence</a> as our wiki. To find out more about Confluence, visit the <a href="http://www.atlassian.com/software/confluence/">Confluence Product Website</a>.</p>
+
+<h3>Mailing Lists</h3>
+<p>There are two mailing lists: the <em>ace-users</em> and the <em>ace-dev</em> mailing lists.</p>
+
+<h4>User List</h4>
+<a href="mailto:ace-users-subscribe@ace.iserver.ch">Subscribe</a>
+<a href="mailto:ace-users-unsubscribe@ace.iserver.ch">Unsubscribe</a>
+<p>This list is for users of the ACE application, to share knowledge, and to discuss issues.</p>
+
+<h4>Developer List</h4>
+<a href="mailto:ace-dev-subscribe@ace.iserver.ch">Subscribe</a>
+<a href="mailto:ace-dev-unsubscribe@ace.iserver.ch">Unsubscribe</a>
+<p>This list is for participating developers of ACE, where we can discuss issues, code changes/additions, etc.</p>
