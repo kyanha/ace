7cfcde1c7a5674acbe948c9854cbe37cb63b3a7b
sim <sim@cc186578-7ef1-0310-b290-876586ca5d03>
2006-03-04 14:30:33 +0000

    removed the huderbaenzes

--- ace/trunk/www/inc/team.inc.php
+++ ace/trunk/www/inc/team.inc.php
@@ -1,17 +1,8 @@
-<h2>Project Team</h2>
+<h2>Developers</h2>
 
 <ul>
  <li>Mark Bigler</li>
  <li>Simon Raess</li>
  <li>Lukas Zbinden</li>
 </ul>
-
-
-<h2>Project Coaching</h2>
-
-<ul>
- <li>Jean-Paul Dubois</li>
- <li>Claude Fuhrer</li>
-</ul>
-
 
