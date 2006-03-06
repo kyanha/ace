9128bcae131c68d4db08f2f7c9c736fb60ce4a97
zbinl <zbinl@cc186578-7ef1-0310-b290-876586ca5d03>
2006-03-06 14:16:47 +0000

    update

--- ace/trunk/www/inc/links.inc.php
+++ ace/trunk/www/inc/links.inc.php
@@ -8,21 +8,13 @@
   <a href="http://www.it.swin.edu.au/personal/yyang/papers/ACMTOCHI1998.pdf" target="_blank">Achieving Convergence, Causality-preservation, and Intention-preservation in Real-time cooperative Editing Systems</a> - Contains a description of the operational transformation functions implemented in ACE.
  </li>
  <li>
-  <a href="http://www.codingmonkeys.de/subethaedit/" target="_blank">SubEthaEdit</a>
-  - Mac OS X based collaborative editor.
- </li>
-   <li>
-  <a href="http://www.cit.gu.edu.au/~scz/sigce/" target="_blank">SIGCE</a>
-  - An interest group on collaborative editing.
- </li>
-  <li>
-  <a href="http://charlie.dgrc.crc.ca/~sylvie/collwrite.html" target="_blank">Collaborative writing</a>
-  - A link collection.
- </li>
- <li>
   <a href="http://www.apple.com/macosx/features/bonjour/" target="_blank">Bonjour</a>
   - Apple's zeroconf networking implementation used by ACE.
  </li>
+ <li>
+  <a href="http://www.beepcore.org/" target="_blank">BEEP</a>
+  - a  network application framework protocol used by ACE.
+ </li> 
 </ul>
 
 
