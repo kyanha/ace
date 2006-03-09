ff75abb4de04b8275264edd8d7cf45cb61292f6e
zbinl <zbinl@cc186578-7ef1-0310-b290-876586ca5d03>
2006-03-09 16:42:18 +0000

    adjusted mailing lists a.o.

--- ace/trunk/www/inc/development.inc.php
+++ ace/trunk/www/inc/development.inc.php
@@ -1,32 +1,19 @@
 <h2>Development</h2>
 
-<p>The source code of the project can be found in our subversion repository.</p>
+<p>The source code of the project can be found in our subversion repository:</p>
 
 <pre class="wiki">
 svn co http://ace.iserver.ch/repos/ace/ace/trunk ace
 </pre>
 
 <h3>Trac</h3>
-<p>In the first stage of the project we used <a href="http://trac.edgewall.com/">trac</a>, a combined wiki, issue tracker, and repository
-browser. Trac is still running, but we do not use it anymore as wiki and
-issue tracker. Trac can be found <a href="http://ace.iserver.ch:81/cgi-bin/trac.cgi">here</a>.</p>
+<p>In the first stage of the project we used <a href="http://ace.iserver.ch/cgi-bin/trac.cgi">Trac</a>, a combined wiki, issue tracker, and repository
+browser. Trac is still running, but we do not use it anymore. Nevertheless, it contains a link collection of all research papers we have considered for ACE. 
+	To find out more about Trac, visit the <a href="http://trac.edgewall.com/" target="_blank">Trac Product Website</a>.</p>
 
 <h3>Jira</h3>
-<p>We switched to <a href="http://ace.iserver.ch/jira">Jira</a> as our
-issue tracker. At the moment, this issue tracker is only used internally to keep track of open tasks, bugs, and enhancements. To find out more about Jira, visit the <a href="http://www.atlassian.com/software/jira/">Jira Product Website.</a>.</p>
+<p>We currently work with <a href="http://ace.iserver.ch/jira">Jira</a> as our
+issue tracker. This issue tracker is used (internally) to keep track of open tasks, bugs, and enhancements. To find out more about Jira, visit the <a href="http://www.atlassian.com/software/jira/" target="_blank">Jira Product Website.</a>.</p>
 
 <h3>Confluence</h3>
-<p>We also use <a href="http://ace.iserver.ch/confluence">Confluence</a> as our wiki. To find out more about Confluence, visit the <a href="http://www.atlassian.com/software/confluence/">Confluence Product Website</a>.</p>
-
-<h3>Mailing Lists</h3>
-<p>There are two mailing lists: the <em>ace-users</em> and the <em>ace-dev</em> mailing lists.</p>
-
-<h4>User List</h4>
-<a href="mailto:ace-users-subscribe@ace.iserver.ch">Subscribe</a>
-<a href="mailto:ace-users-unsubscribe@ace.iserver.ch">Unsubscribe</a>
-<p>This list is for users of the ACE application, to share knowledge, and to discuss issues.</p>
-
-<h4>Developer List</h4>
-<a href="mailto:ace-dev-subscribe@ace.iserver.ch">Subscribe</a>
-<a href="mailto:ace-dev-unsubscribe@ace.iserver.ch">Unsubscribe</a>
-<p>This list is for participating developers of ACE, where we can discuss issues, code changes/additions, etc.</p>
+<p>We use <a href="http://ace.iserver.ch/confluence">Confluence</a> as our wiki. To find out more about Confluence, visit the <a href="http://www.atlassian.com/software/confluence/">Confluence Product Website</a>.</p>
--- ace/trunk/www/inc/mailinglists.inc.php
+++ ace/trunk/www/inc/mailinglists.inc.php
@@ -1,12 +1,11 @@
 <h2>ACE Mailing Lists</h2>
 
-The ACE project has two mailing lists.
+The ACE project has two mailing lists: the <em>ace-announce</em> and the <em>ace-devel</em> mailing lists.</p>
 
-<h3>User List</h3>
-<a href="mailto:ace-users-subscribe@ace.iserver.ch">subscribe</a>
-<a href="mailto:ace-users-unsubscribe@ace.iserver.ch">unsubscribe</a>
-<p>The user list is primarily for (potential) users of ACE.</p>
+<h3>User List <em>ace-announce</em></h3>
+<p>This is a read only low traffic mailing list for announcements and new releases.</p>
+<a href="http://lists.sourceforge.net/lists/listinfo/ace-announce">Subscribe/Unsubscribe/Preferences</a>
 
-<h3>Developers List</h3>
-<a href="mailto:ace-dev-subscribe@ace.iserver.ch">subscribe</a>
-<a href="mailto:ace-dev-unsubscribe@ace.iserver.ch">unsubscribe</a><p>The developers list is used by developers of ACE to discuss the design and implementation of ACE.</p>
+<h3>Developer List <em>ace-devel</em></h3>
+<p>The developer list is used by developers of ACE to discuss the design and implementation of ACE.</p>
+<a href="http://lists.sourceforge.net/lists/listinfo/ace-devel">Subscribe/Unsubscribe/Preferences</a>
