8b4b302e15ef84cad48f1855835363a7f19d0ef4
zbinl <zbinl@cc186578-7ef1-0310-b290-876586ca5d03>
2007-01-02 23:42:51 +0000

    initial checkin

---
+++ prototypes/trunk/network/jini/src/java/ch/iserver/ace/net/jdiscovery/CheckPeerAliveWorker.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package ch.iserver.ace.net.jdiscovery;
+
+import java.rmi.RemoteException;
+
+/**
+ * @author Luke
+ *
+ */
+public class CheckPeerAliveWorker extends Thread {
+
+	private final IDiscoveryListener peer;
+	private final String peerId;
+	
+	public CheckPeerAliveWorker(String peerId, IDiscoveryListener peer) {
+		this.peer = peer;
+		this.peerId = peerId;
+	}
+	
+	
+	public void run() {
+		LogUtil.print("CheckPeerAliveWorker: checks peer " + peerId + " alive...");
+		try {
+			peer.checkAlive();
+		} catch (RemoteException e) {
+			LogUtil.print("peer: " + peerId + " disappeared.");
+			LogUtil.print(e.toString());
+			RegistrationLookupMediator.getInstance().serviceLoggedOut(peerId);
+		}
+		
+	}
+}
